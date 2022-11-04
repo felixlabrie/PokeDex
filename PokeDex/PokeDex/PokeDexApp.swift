38cb7c2241c57b4ab4e16a4ad2cc996b6adeb67b
Félix Étienne Labrie <48634914+felixlabrie@users.noreply.github.com>
2022-11-04 10:21:05 -0400

    initial commit

---
+++ PokeDex/PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Félix Étienne Labrie on 2022-11-04.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
